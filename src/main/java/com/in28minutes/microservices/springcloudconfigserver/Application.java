6fe57481a479833a08a9be3b8ce9376b484a8da4
Thúlio Sousa <thuliomattheus@hotmail.com>
2022-02-22 08:40:08 -0300

    inicializando repo

---
+++ src/main/java/com/in28minutes/microservices/springcloudconfigserver/Application.java
@@ -0,0 +1,14 @@
+package com.in28minutes.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
